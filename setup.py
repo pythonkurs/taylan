320138cce201db313c19602389915ff3f55c4af1
Fulya Taylan <fulya@danielmba.scilifelab.se>
2013-01-23 13:04:40 +0100

    First Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fulyabiostuff',
+    version='0.1dev',
+    author='Fulya Taylan',
+    author_email='fulyataylan@gmail.com',
+    url='example.com',
+    packages=['biostuff',],
+    license='GPLv3',
+    long_description=open('README.txt').read(),
+)
